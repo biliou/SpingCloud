a2b3776af423846ed3fb1e0accd6d51d13fd7f48
bili.ou <bili.ou@cyphertech.cn>
2017-10-23 17:07:39 +0800

    添加 服务链路追踪 Demo

--- service-consumer/src/main/java/com/example/controller/ConsumerController.java
+++ service-consumer/src/main/java/com/example/controller/ConsumerController.java
@@ -1,15 +1,25 @@
 package com.example.controller;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConsumerController {
+	
+	 private static final Logger LOG = Logger.getLogger(ConsumerController.class.getName());
 
     @Autowired
     HelloRemote HelloRemote;
+    
+    @Value("${server.port}")
+	String port;
 	
     /**
      * 通过 http://localhost:9003/hello/neo 访问
@@ -18,7 +28,18 @@ public class ConsumerController {
      */
     @RequestMapping("/hello/{name}")
     public String index(@PathVariable("name") String name) {
+    	LOG.log(Level.INFO, "===================================================================calling trace service-consumer  " + port);
+    	
         return HelloRemote.hello(name);
+    }
+    
+    /**
+	 * zipkin 固有方法
+	 * @return
+	 */
+	@Bean
+    public AlwaysSampler defaultSampler(){
+        return new AlwaysSampler();
     }
 
 }
--- service-producter1/src/main/java/com/example/controller/MainController.java
+++ service-producter1/src/main/java/com/example/controller/MainController.java
@@ -1,13 +1,21 @@
 package com.example.controller;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.Producer1Application;
+
 
 @RestController
 public class MainController {
+	
+	private static final Logger LOG = Logger.getLogger(Producer1Application.class.getName());
 	
 	@Value("${server.port}")
 	String port;
@@ -19,6 +27,8 @@ public class MainController {
 	 */
 	@RequestMapping("/hello")
     public String index(@RequestParam String name) {
+		LOG.log(Level.INFO, "===================================================================calling trace service-producer "+ port);
+		
         return "hello "+name+"，this is producer 2  send first messge, from " + port;
     }
 	
@@ -30,5 +40,14 @@ public class MainController {
 	@RequestMapping("/zuul")
     public String testZuul(@RequestParam String name) {
         return "hello "+name+"，this is one message";
+    }
+	
+	/**
+	 * zipkin 固有方法
+	 * @return
+	 */
+	@Bean
+    public AlwaysSampler defaultSampler(){
+        return new AlwaysSampler();
     }
 }
